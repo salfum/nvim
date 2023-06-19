50dbe8d464f70f7705637f85e70bf0dfc47d4445
Maksim Ermakov <salfum@protonmail.com>
2023-06-19 10:32:02 +0700

    refactor: rewrite vim commands calling

--- mappings.lua
+++ mappings.lua
@@ -11,9 +11,11 @@ function Hard_remap_mappings()
   utils.set_mappings(astronvim.user_opts("mappings", mappings_list))
 end
 
+local function cmd(str) return "<cmd>" .. str .. "<CR>" end
+
 return {
   n = {
-    ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
+    ["<leader>bn"] = { cmd "tabnew", desc = "New tab" },
     ["<leader>bD"] = {
       function()
         require("astronvim.utils.status").heirline.buffer_picker(
@@ -23,36 +25,36 @@ return {
       desc = "Pick to close",
     },
     ["<leader>b"] = { name = "Buffers" },
-    ["<Tab>"] = { "<cmd>bnext<cr>" },
-    ["<S-Tab>"] = { "<cmd>bprevious<cr>" },
-    ["<Leader>fd"] = { "<cmd>Zeavim<cr>", desc = "Find documentation" },
+    ["<Tab>"] = { cmd "bnext" },
+    ["<S-Tab>"] = { cmd "bprevious" },
+    ["<Leader>fd"] = { cmd "Zeavim", desc = "Find documentation" },
 
     -- Telescope mappings
-    ["<Leader>fB"] = { "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Find fuzzy in current buffer" },
-    ["<Leader>fz"] = { "<cmd>Telescope zoxide list<cr>", desc = "Find zoxide" },
-    ["<Leader>fw"] = { "<cmd>Telescope grep_string<cr>", desc = "Find string under cursor" },
-    ["<Leader>fg"] = { "<cmd>Telescope live_grep<cr>", desc = "Find grep" },
-    ["<Leader>fG"] = { "<cmd>Telescope live_grep hidden=true<cr>", desc = "Find grep with hidden" },
-    ["<Leader>fr"] = { "<cmd>Telescope oldfiles cwd_only=true<cr>", desc = "Find recent files" },
-    ["<Leader>fR"] = { "<cmd>Telescope oldfiles cwd_only=true hidden=true<cr>", desc = "Find recent files with hidden" },
-    ["<Leader>fO"] = { "<cmd>Telescope registers<CR>", desc = "Find registers" },
-    ["<Leader>fc"] = { "<cmd>Telescope neoclip a extra=star,plus,b<cr>", desc = "Find clipboard" },
-    ["<Leader>fs"] = { "<cmd>Telescope git_status<cr>", desc = "Find git status" },
+    ["<Leader>fB"] = { cmd "Telescope current_buffer_fuzzy_find", desc = "Find fuzzy in current buffer" },
+    ["<Leader>fz"] = { cmd "Telescope zoxide list", desc = "Find zoxide" },
+    ["<Leader>fw"] = { cmd "Telescope grep_string", desc = "Find string under cursor" },
+    ["<Leader>fg"] = { cmd "Telescope live_grep", desc = "Find grep" },
+    ["<Leader>fG"] = { cmd "Telescope live_grep hidden=true", desc = "Find grep with hidden" },
+    ["<Leader>fr"] = { cmd "Telescope oldfiles cwd_only=true", desc = "Find recent files" },
+    ["<Leader>fR"] = { cmd "Telescope oldfiles cwd_only=true hidden=true", desc = "Find recent files with hidden" },
+    ["<Leader>fO"] = { cmd "Telescope registers", desc = "Find registers" },
+    ["<Leader>fc"] = { cmd "Telescope neoclip a extra=star,plus,b", desc = "Find clipboard" },
+    ["<Leader>fs"] = { cmd "Telescope git_status", desc = "Find git status" },
     ["<Leader>fa"] = {
-      "<cmd>Telescope agrolens query=functions,callings,comments,labels buffers=all<cr>",
+      cmd "Telescope agrolens query=functions,callings,comments,labels buffers=all",
       desc = "Find TS query",
     },
 
     -- Renames all references to the symbol under the cursor
-    ["<F2>"] = { '<cmd>lua require("cosmic-ui").rename()<cr>' },
+    ["<F2>"] = { cmd "lua require('cosmic-ui').rename()" },
     -- Selects a code action available at the current cursor position
-    ["<F4>"] = { '<cmd>lua require("cosmic-ui").code_actions()<cr>' },
+    ["<F4>"] = { cmd "lua require('cosmic-ui').code_actions()" },
   },
   x = {
-    ["<F4>"] = { '<cmd>lua require("cosmic-ui").range_code_actions()<cr>' },
+    ["<F4>"] = { cmd "lua require('cosmic-ui').range_code_actions()" },
   },
   i = {
-    ["<C-s>"] = { "<cmd>write<cr>" },
+    ["<C-s>"] = { cmd "write" },
     ["<C-e>"] = { "<End>" },
     ["<C-a>"] = { "<Home>" },
   },
